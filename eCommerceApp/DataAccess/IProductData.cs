c8554f723dfc12f755edbd84a648199065c62191
Stbergeson <sbergeson1@gmail.com>
2022-12-31 14:09:44 -0600

    add structure for product data access

---
+++ eCommerceApp/DataAccess/IProductData.cs
@@ -0,0 +1,12 @@
+﻿using eCommerceApp.Models;
+
+namespace eCommerceApp.DataAccess
+{
+    public interface IProductData
+    {
+        Task<List<Category>> GetCategories();
+        Task<List<Image>> GetImages(int productId);
+        Task<List<Product>> GetProducts(int subCategoryId);
+        Task<List<SubCategory>> GetSubCategories(int categoryId);
+    }
+}
---
+++ eCommerceApp/DataAccess/ProductData.cs
@@ -0,0 +1,34 @@
+﻿using eCommerceApp.Models;
+using Microsoft.CodeAnalysis.Elfie.Model.Tree;
+
+namespace eCommerceApp.DataAccess;
+
+public class ProductData : IProductData
+{
+    private readonly ISqlDataAccess _sql;
+
+    public ProductData(ISqlDataAccess sql)
+    {
+        _sql = sql;
+    }
+
+    public Task<List<Category>> GetCategories()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<SubCategory>> GetSubCategories(int categoryId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Product>> GetProducts(int subCategoryId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Image>> GetImages(int productId)
+    {
+        throw new NotImplementedException();
+    }
+}
