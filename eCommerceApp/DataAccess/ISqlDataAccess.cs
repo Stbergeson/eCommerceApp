9c918ba3aa622f76c86c362d635a47aefa3c5876
Stbergeson <sbergeson1@gmail.com>
2022-12-31 13:32:11 -0600

    added sql access class

---
+++ eCommerceApp/DataAccess/ISqlDataAccess.cs
@@ -0,0 +1,8 @@
+﻿namespace eCommerceApp.DataAccess
+{
+    public interface ISqlDataAccess
+    {
+        Task<List<T>> LoadData<T, U>(string sql, U parameters, string connectionStringName = "Default");
+        Task SaveData<T>(string sql, T paramters, string connectionStringName = "Default");
+    }
+}
---
+++ eCommerceApp/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace eCommerceApp.DataAccess;
+
+public class SqlDataAccess : ISqlDataAccess
+{
+    private readonly IConfiguration _configuration;
+    public SqlDataAccess(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public async Task<List<T>> LoadData<T, U>(string sql, U parameters, string connectionStringName = "Default")
+    {
+        string connectionString = _configuration.GetConnectionString(connectionStringName)!;
+
+        using IDbConnection connection = new SqlConnection(connectionString);
+
+        var result = await connection.QueryAsync<T>(sql, parameters);
+
+        return result.ToList();
+    }
+
+    public async Task SaveData<T>(string sql, T paramters, string connectionStringName = "Default")
+    {
+        string connectionString = _configuration.GetConnectionString(connectionStringName)!;
+
+        using IDbConnection connection = new SqlConnection(connectionString);
+
+        await connection.ExecuteAsync(sql, paramters);
+    }
+}
