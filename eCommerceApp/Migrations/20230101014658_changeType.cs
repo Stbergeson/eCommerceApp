4649db9495ee35c209c7891fcee9876b79bf7822
Stbergeson <sbergeson1@gmail.com>
2022-12-31 19:51:45 -0600

    update model and db

---
+++ eCommerceApp/Migrations/20230101014658_changeType.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eCommerceApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "Products",
+                type: "REAL",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Categories",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Categories");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "Products",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "REAL");
+        }
+    }
+}
--- eCommerceApp/Models/Product.cs
+++ eCommerceApp/Models/Product.cs
@@ -6,7 +6,7 @@ public class Product
     public string ProductName { get; set; }
     public string ProductDescription { get; set; }
     public int SubCategoryId { get; set; }
-    public int Price { get; set; }
+    public double Price { get; set; }
     public bool IsSale { get; set; }
     public int DiscountPercent { get; set; }
 }
